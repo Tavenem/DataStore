65c948013fa6b87ecab19dd6379f316b0ef7ec89
Wil Stead <wil.stead@gmail.com>
2022-08-03 15:43:45 -0400

    Remove IdItemTypeName

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.31.0-preview
+### Changed
+- Update to .NET 7 preview
+- Remove `IdItemTypeName` in favor of built-in polymorphic (de)serialization added to System.Text.Json
+
 ## 0.30.0-preview
 ### Changed
 - Update to .NET 6 preview
--- src/IIdItem.cs
+++ src/IIdItem.cs
@@ -11,9 +11,6 @@
 /// It uses a <see cref="string"/> key which may or may not be unique, depending on your
 /// persistence requirements.
 /// </para>
-/// <para>
-/// It also has a built-in, read-only type discriminator property.
-/// </para>
 /// </remarks>
 public interface IIdItem : IEquatable<IIdItem>
 {
@@ -21,29 +18,4 @@ public interface IIdItem : IEquatable<IIdItem>
     /// The ID of this item.
     /// </summary>
     string Id { get; }
-
-    /// <summary>
-    /// <para>
-    /// A built-in, read-only type discriminator.
-    /// </para>
-    /// <para>
-    /// This property has a default implementation in the <see cref="IIdItem"/> interface which
-    /// uses reflection to generate a string with the format ":<c>GetType().Name</c>:".
-    /// </para>
-    /// <para>
-    /// The property can (and should) be overridden in implementations to hard-code the
-    /// discriminator value, both in order to avoid reflection and also to guard against
-    /// potential breaking changes if a type is renamed.
-    /// </para>
-    /// </summary>
-    /// <remarks>
-    /// <para>
-    /// Inheritance and polymorphism can be modeled by chaining subtypes with the ':' character
-    /// as a separator.
-    /// </para>
-    /// <para>
-    /// For example: ":BaseType:ChildType:".
-    /// </para>
-    /// </remarks>
-    public string IdItemTypeName => $":{GetType().Name}:";
 }
